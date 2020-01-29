8b1d6f69cac7558c1afae93421c2d0e5ae96d754
lovish.sachdeva <lovish@amplify.ai>
2020-01-29 16:37:29 +0530

    segment tree

--- Spoj/GSS.py
+++ Spoj/GSS.py
@@ -1,44 +1,74 @@
 from sys import stdin
+from math import ceil, log2
+
+def getMid(s, e):
+    return s + (e-s)//2
 
 
-def calculateMax(n, arr):
-    dp = [0]*n
-    for i in range(n):
-        if(i==0):
-            dp[i] = arr[i]
-        elif(arr[i] < 0):
-            if(arr[i] > dp[i-1]):
-                dp[i] = arr[i]
-            else:
-                dp[i] = dp[i-1]
-        else:
-            if(dp[i-1] >0):
-                dp[i]  = arr[i] +dp[i-1]
-            else:
-                dp[i] = arr[i]
+def constructSTUtil(arr, ss, se, st, si):
 
-    return dp
+    if (ss == se):
+        st[si] = arr[ss];
+        return arr[ss];
 
-def process(start, end, dp, arr):
-    if (dp[end] <= 0):
-        return max(arr[start: end + 1])
-    if (start == 0 or dp[start -1] <0):
-        return dp[end]
+    mid = getMid(ss, se);
 
-    return dp[end] - dp[start - 1]
+    left =constructSTUtil(arr, ss, mid, st, si * 2 + 1)
+    right = constructSTUtil(arr, mid + 1, se, st, si * 2 + 2)
+    if(left and right):
+        st[si] = max(left, right, left+ right)
+    elif(left):
+        st[si] = left
+    else:
+        st[si] = right
+
+    return st[si];
 
 
+def constructST(arr, n):
 
+    x = (int)(ceil(log2(n)));
+
+
+    max_size = 2 * (int)(2 ** x) - 1;
+
+
+    st = [None] * max_size;
+
+    constructSTUtil(arr, 0, n - 1, st, 0);
+    return st;
+
+
+def getSumUtil(st, ss, se, qs, qe, si):
+
+    if (qs <= ss and qe >= se):
+        return st[si];
+
+    if (se < qs or ss > qe):
+        return None
+
+    mid = getMid(ss, se);
+    left = getSumUtil(st, ss, mid, qs, qe, 2 * si + 1)
+    right = getSumUtil(st, mid + 1, se, qs, qe, 2 * si + 2);
+    if(left and right):
+        return max(left,right, left+right)
+    elif(left):
+        return left
+    else:
+        return right
+
+
+
+
+
+def getSum(st, n, qs, qe):
+    return getSumUtil(st, 0, n - 1, qs, qe, 0);
 
 if __name__ == "__main__":
     n = int(input())
     arr  = list(map(int, stdin.readline().split()))
-    source = calculateMax(n, arr)
-    print("{}".format(source))
-    q  = int(input())
-    for i in range(q):
+    st = constructST(arr, n)
+
+    for i in range(int(input())):
         x, y = list(map(int, stdin.readline().split()))
-        if(x == y):
-            print(arr[x-1])
-        else:
-            print(process(x-1, y-1, source, arr))
+        print(getSum(st,n, x-1, y-1))
